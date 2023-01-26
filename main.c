5b5cbc12e5d2a21c8254f6615364d1c8301420f3
Hemcode <pro.hemdy@gmail.com>
2023-01-26 17:21:26 +0100

    c_002

--- main.c
+++ main.c
@@ -6,13 +6,23 @@ struct Livre
 	char titre[20];
 	char auteur[20];
 	int year;
+	
 };
+
+void presentation();
 
 int main(void)
 {
 	struct Livre livre1;
-	strcpy(livre1.titre, "Harry Potter");
-	strcpy(livre1.auteur, "J.K. Rolling");
+	strcpy(livre1.titre,/* Nom du livre */ "Harry Potter");
+	strcpy(livre1.auteur,/* Nom de l'auteur */ "J.K. Rowling");
 	livre1.year = 1997;
+	
+	presentation(livre1);
 	return 0;
+}
+
+void presentation(struct Livre object)
+{
+	printf("Le nom du livre est %s, l'auteur est %s et il date de %d.\n", object.titre, object.auteur, object.year);
 }
