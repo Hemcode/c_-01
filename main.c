a5ae548e0a95012bf4228a9b73e791cf5a514754
Hemcode <pro.hemdy@gmail.com>
2023-01-26 18:38:04 +0100

    c_005

--- main.c
+++ main.c
@@ -1,42 +1,11 @@
 #include <stdio.h>
-#include <string.h>
-
-typedef struct {char titre[35]; char auteur[20]; int year;} Object;
-
-void presentation();
 
 int main(void)
 {
-	Object livres[3];
+	int num = 115;
+	int* pnum = &num; // Les pointeurs sont des constantes.
 	
-	strcpy(livres[0].titre, "Hello World");
-	strcpy(livres[0].auteur, "Hemdy Mameche");
-	livres[0].year = 2029;
+	printf("La valeur stocke dans %x est %d", pnum, *pnum);
 	
-	strcpy(livres[1].titre, "Le seigneur des annaux");
-	strcpy(livres[1].auteur, "J.R.R. Tolkien");
-	livres[1].year = 1954;
-	
-	strcpy(livres[2].titre, "The Shining");
-	strcpy(livres[2].auteur, "Stephen King");
-	livres[2].year = 1977;
-	
-	strcpy(livres[3].titre, "IT");
-	strcpy(livres[3].auteur, "Stephen King");
-	livres[3].year = 1986;
-	
-	for (int i = 0; i <= 3; i++)
-	{
-		presentation(livres[i]);
-	}
 	return 0;
-}
-
-void presentation(Object object)
-{
-	printf("==================\n");
-	printf("Auteur: %s\n", object.auteur);
-	printf("Titre : %s\n", object.titre);
-	printf("Annee : %d\n", object.year);
-	printf("==================\n");
 }
