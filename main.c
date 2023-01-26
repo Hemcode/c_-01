b792d15b5485ecee6a484d70b1b0b21803ebbae4
Hemcode <pro.hemdy@gmail.com>
2023-01-26 20:35:59 +0100

    c_006

--- main.c
+++ main.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 
+typedef struct 
+	{
+		char name[15];
+		int age;
+	} Personne;
+
+int estMajeur(Personne personne);
+void sePresenter(Personne personne);
+
 int main(void)
 {
-	int num = 115;
-	int* pnum = &num; // Les pointeurs sont des constantes.
+	Personne personne1;
+	strcpy(personne1.name, "Hemdy");
+	personne1.age = 13;
 	
-	printf("La valeur stocke dans %x est %d", pnum, *pnum);
-	
+	sePresenter(personne1);
 	return 0;
+}
+
+int estMajeur(Personne personne)
+{
+	return personne.age >= 18;
+}
+
+void sePresenter(Personne personne)
+{
+	printf("Je m'appelle %s et j'ai %d ans.\n", personne.name, personne.age);
+	printf("estMajeur: %d\n", estMajeur(personne));
 }
