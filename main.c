206220cb5d04edde5539b660a91dbe01125971e9
Hemcode <pro.hemdy@gmail.com>
2023-01-26 20:45:47 +0100

    c_007

--- main.c
+++ main.c
@@ -11,11 +11,20 @@ void sePresenter(Personne personne);
 
 int main(void)
 {
-	Personne personne1;
-	strcpy(personne1.name, "Hemdy");
-	personne1.age = 13;
+	Personne personnes[2];
+	int nbPersonnes = 0;
 	
-	sePresenter(personne1);
+	strcpy(personnes[0].name, "Hemdy");
+	personnes[0].age = 13;
+	nbPersonnes += 1;
+	
+	strcpy(personnes[1].name, "Soulaymane");
+	personnes[1].age = 19;
+	nbPersonnes += 1;
+	
+	for (int i = 0; i < nbPersonnes/* nbPersonnes est exclu de la liste. */; i++)
+		sePresenter(personnes[i]);
+	
 	return 0;
 }
 
@@ -26,6 +35,8 @@ int estMajeur(Personne personne)
 
 void sePresenter(Personne personne)
 {
+	printf("=======================================\n");
 	printf("Je m'appelle %s et j'ai %d ans.\n", personne.name, personne.age);
 	printf("estMajeur: %d\n", estMajeur(personne));
+	printf("=======================================\n");
 }
