f47829a4921d4a12d378859f93850448c535dc98
Hemcode <pro.hemdy@gmail.com>
2023-01-27 20:02:35 +0100

    c_009

--- main.c
+++ main.c
@@ -1,23 +1,7 @@
 #include <stdio.h>
 
-void swap(int* x, int* y);
-
 int main(void)
 {
-	int n1 = 12;
-	int n2 = 81;
-	printf("n1 = %d | n2 = %d\n", n1, n2);
-	
-	printf("Swap...\n");
-	swap(&n1, &n2);
-	
-	printf("n1 = %d | n2 = %d\n", n1, n2);
+	printf("Hello World\n");
 	return 0;
-}
-
-void swap(int* x, int* y)
-{
-	int tmp = *x;
-	*x = *y;
-	*y = tmp;
 }
