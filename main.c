d2a9d37dfb537446b98341cbbf888dd6c34c0ed1
Hemcode <pro.hemdy@gmail.com>
2023-01-27 20:24:14 +0100

    c_010

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-	printf("Hello World\n");
+	int* pnum = (int*) malloc(sizeof(int));
+	*pnum = 15;
+	
+	printf("Value   : %d\n", *pnum);
+	printf("Adresse : %x\n", pnum);
+	
+	free(pnum);
 	return 0;
 }
